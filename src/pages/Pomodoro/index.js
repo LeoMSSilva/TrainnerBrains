1a3ec86c04ffd648b673ff0f182e935a1e7a94fe
LeoMSSilva <l.eom1994@hotmail.com>
2021-10-30 19:49:23 -0300

    Creation of the buttonPlayer component

--- src/pages/Pomodoro/index.js
+++ src/pages/Pomodoro/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import ButtonPlayer from '../../components/ButtonPlayer';
+
+import * as Styled from './styles';
 
 const Pomodoro = () => {
-  return <View></View>;
+  const [status, setStatus] = useState(true);
+
+  return (
+    <Styled.Container>
+      <ButtonPlayer
+        name={status ? 'pause' : 'play'}
+        onPress={() => {
+          setStatus(!status);
+        }}
+      />
+      <Styled.Timer>25:00</Styled.Timer>
+      <Styled.Status>{status ? 'Focus' : 'Pause'}</Styled.Status>
+    </Styled.Container>
+  );
 };
 
 export default Pomodoro;
---
+++ src/pages/Pomodoro/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  width: 100%;
+  height: 100%;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #edf5f7;
+`;
+
+export const Timer = styled.Text`
+  font-size: 32px;
+  font-weight: 700;
+  color: #0c4b93;
+`;
+
+export const Status = styled.Text`
+  font-size: 32px;
+  font-weight: 700;
+  color: #0c4b93;
+`;
