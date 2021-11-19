58c4885617eb41ecab14a04fa8bdc0d82ab01704
LeoMSSilva <l.eom1994@hotmail.com>
2021-11-19 18:59:07 -0300

    Inclusion of word swap logic

--- src/pages/FastReading/index.js
+++ src/pages/FastReading/index.js
@@ -1,15 +1,42 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ButtonGoBack from '../../components/ButtonGoBack';
 import ButtonPlayer from '../../components/ButtonPlayer';
+import Input from '../../components/Input';
+import SpeedControl from '../../components/SpeedControl';
+import TextRead from '../../components/TextRead';
 import Title from '../../components/Title';
 import * as Styled from './styles';
 
 const FastReading = () => {
   const [status, setStatus] = useState(true);
+  const [time, setTime] = useState(1);
+  const [text, setText] = useState('Texto de exemplo');
+  const [words, setWords] = useState([]);
+  const [currentWord, setCurrentWord] = useState(' ');
+  const [positionWordInWords, setPositionWordInWords] = useState(0);
+
+  useEffect(() => setWords(text.split(' ')), [text]);
+
+  const nextPositionArray = () => {
+    const interval = setInterval(() => {
+      setCurrentWord(words[positionWordInWords]);
+      console.log('currentWord ' + currentWord);
+      console.log('positionWordInWords ' + positionWordInWords);
+      positionWordInWords < words.length &&
+        setPositionWordInWords(positionWordInWords + 1);
+      clearInterval(interval);
+    }, time * 1000);
+  };
 
   const handleRead = () => {
     setStatus(!status);
+    nextPositionArray();
   };
+
+  //next time pomodoro
+  useEffect(() => {
+    positionWordInWords < words.length && nextPositionArray();
+  }, [positionWordInWords]);
 
   return (
     <Styled.Container>
@@ -17,6 +44,15 @@ const FastReading = () => {
         <ButtonGoBack />
         <Title text="FastReadingTrainner" />
       </Styled.Row>
+      {status ? (
+        <Input
+          placeholder="Cole aqui o texto"
+          setValue={setText}
+          value={text}
+        />
+      ) : (
+        <TextRead text={currentWord} />
+      )}
       <ButtonPlayer
         size={70}
         name={status ? 'play' : 'pause'}
