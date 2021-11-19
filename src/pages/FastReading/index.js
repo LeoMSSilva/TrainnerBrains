6520b2eb6e1dc4382c73678340a11de8aa154b14
LeoMSSilva <l.eom1994@hotmail.com>
2021-11-19 18:51:39 -0300

    Adjust in buttonPlayer component

--- src/pages/FastReading/index.js
+++ src/pages/FastReading/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import Title from '../../components/Title';
+import React, {useState} from 'react';
 import ButtonGoBack from '../../components/ButtonGoBack';
+import ButtonPlayer from '../../components/ButtonPlayer';
+import Title from '../../components/Title';
 import * as Styled from './styles';
 
 const FastReading = () => {
+  const [status, setStatus] = useState(true);
+
+  const handleRead = () => {
+    setStatus(!status);
+  };
 
   return (
     <Styled.Container>
@@ -11,6 +17,11 @@ const FastReading = () => {
         <ButtonGoBack />
         <Title text="FastReadingTrainner" />
       </Styled.Row>
+      <ButtonPlayer
+        size={70}
+        name={status ? 'play' : 'pause'}
+        onPress={handleRead}
+      />
     </Styled.Container>
   );
 };
