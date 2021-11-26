db3d48e53b091b3a9b62f5e8fb40c429cf850c46
LeoMSSilva <l.eom1994@hotmail.com>
2021-11-26 08:29:58 -0300

    Refactor fixed strategy using division of responsibilities

--- src/pages/Pomodoro/index.js
+++ src/pages/Pomodoro/index.js
@@ -1,47 +1,45 @@
 import React, {useEffect, useState} from 'react';
-import ButtonPlayer from '../../components/ButtonPlayer';
+import ButtonGoBack from '../../components/ButtonGoBack';
 import Title from '../../components/Title';
 import Timer from '../../components/Timer';
+import ButtonPlayer from '../../components/ButtonPlayer';
 import * as Styled from './styles';
-import ButtonGoBack from '../../components/ButtonGoBack';
 
 const Pomodoro = () => {
   const [status, setStatus] = useState(true);
-  const pomodoro = [25, 5, 25, 5, 25, 5, 25, 15];
+  // [25*60, 5*60, 25*60, 5*60, 25*60, 5*60, 25*60, 15*60];
+  const pomodoro = [1500, 300, 1500, 300, 1500, 300, 1500, 900];
   const [positionTimer, setPositionTimer] = useState(0);
   const [currentTime, setCurrentTime] = useState();
   const [timer, setTimer] = useState();
 
-  //start pomodoro
-  useEffect(() => setCurrentTime(pomodoro[positionTimer] * 60), []);
-
-  //stop before the negative number
-  useEffect(() => {
-    return () => clearInterval(timer);
-  }, [timer]);
+  const decrement = () => {
+    const interval = setInterval(() => {
+      status && setCurrentTime(currentTime => currentTime - 1);
+      clearInterval(timer);
+    }, 1000);
+    setTimer(interval);
+  };
 
   const handlePomodoro = () => {
     setStatus(!status);
-    const timer = setInterval(() => {
-      status && setCurrentTime(currentTime => currentTime - 1);
-      currentTime === 0 && clearInterval(timer);
-    }, 1000);
-    setTimer(timer);
+    decrement();
   };
 
-  //next time pomodoro
+  const isFinalWords = () =>
+    positionTimer + 1 < pomodoro.length ? positionTimer + 1 : 0;
+
+  //next position time in pomodoro
   useEffect(() => {
     if (currentTime === 0) {
       clearInterval(timer);
       setStatus(!status);
-
-      setPositionTimer(positionTimer === 7 ? 0 : positionTimer + 1);
-      setCurrentTime(pomodoro[positionTimer] * 60);
-      setCurrentTime(
-        pomodoro[positionTimer === 7 ? 0 : positionTimer + 1] * 60,
-      );
+      setPositionTimer(isFinalWords);
     }
   }, [currentTime]);
+
+  //next time in pomodoro
+  useEffect(() => setCurrentTime(pomodoro[positionTimer]), [positionTimer]);
 
   return (
     <Styled.Container>
