b47bc07855e7c6dcba67bc00d37e7e49e2e08d9a
LeoMSSilva <l.eom1994@hotmail.com>
2023-02-08 17:23:46 -0300

    feat: created the dark theme and applied switch between light or dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import { ThemeProvider } from 'styled-components/native';
 
 import { Routes } from './routes';
-import { theme } from './theme';
+import { dark, light } from './theme';
 
 export const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [isDark, setIsDark] = useState(isDarkMode);
+
+  useEffect(() => setIsDark(isDarkMode), [isDarkMode]);
+
   useEffect(() => {
     setTimeout(() => SplashScreen.hide(), 1000);
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDark ? dark : light}>
       <StatusBar
-        barStyle="dark-content"
+        barStyle={isDark ? 'light-content' : 'dark-content'}
         backgroundColor="transparent"
         translucent
       />
